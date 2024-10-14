2d305cd82f4f48d82f2c08821645f2a0dc9204d2
Mubashir-Sultan062 <hmubashir062@gmail.com>
2024-10-14 14:42:16 +0500

    Add files via upload

---
+++ swaping number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// initial phase
+	float a,b;
+	// processing phase
+	cout << "swap two numbers: " << endl << endl << "---------------------" << endl;
+	cout << "input fisrt number: ";
+	cin >> a;
+	cout << "input second number: " ;
+	cin >> b;
+	cout << "after swapping of the fisrt number is: " << b << endl;
+	cout << "after swapping of the second number is:" << a<< endl;
+	// termination phase
+	return 0;
+	
+}
